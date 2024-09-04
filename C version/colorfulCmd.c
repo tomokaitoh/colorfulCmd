2718fbabc84b27be5ff0e3a6bdeae36c0af235ad
tomoka-itoh <70480435+tomokaitoh@users.noreply.github.com>
2024-09-04 21:52:00 +0800

    Add files via upload

---
+++ C version/colorfulCmd.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+
+#define BITMAP_FILE_PATH "furina.bmp"  // Bitmap文件的路径
+#define CHARACTER "#"  // 在控制台用来显示图片的字符
+
+struct Pixel {
+    unsigned char B,G,R;
+};
+
+struct Pixel *pixels = NULL;    // 图片的所有像素
+int width, height;              // 图片的 宽度 和 高度
+
+
+// 开启Windows的虚拟终端序列支持
+int enableVTMode() {
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut == INVALID_HANDLE_VALUE)
+        return -1;
+
+    DWORD dwMode = 0;
+    if (!GetConsoleMode(hOut, &dwMode))
+        return -2;
+
+    dwMode |= 0x0004;
+    if (!SetConsoleMode(hOut, dwMode))
+        return -3;
+    return 1;
+}
+
+
+// 读取Bitmap图片
+int readBitmap(const char* filePath) {
+    FILE *file = fopen(filePath, "rb");
+    if(file == NULL)
+        return -1;
+
+    // 读取Bitmap的文件头部分
+    BITMAPFILEHEADER fileHeader;
+    BITMAPINFOHEADER infoHeader;
+    fread(&fileHeader, sizeof(BITMAPFILEHEADER), 1, file);
+    fread(&infoHeader, sizeof(BITMAPINFOHEADER), 1, file);
+    width = infoHeader.biWidth;
+    height = infoHeader.biHeight;
+
+    // 读取Bitmap图片的数据
+    pixels = malloc(sizeof(struct Pixel) * width * height);
+    fread(pixels, sizeof(struct Pixel), width*height, file);
+
+    // Bitmap图片的行顺序是上下颠倒的，这里把它颠倒回来
+    struct Pixel* temp = malloc(sizeof(struct Pixel) * width * height);
+    for (int y = 0; y < height; y++)
+        memcpy(temp+y*width, pixels+(height-y-1)*width, width*sizeof(struct Pixel));
+    free(pixels);
+    pixels = temp;
+
+    fclose(file);
+    return 1;
+}
+
+int main(int argc, char *argv[]){
+
+    // 开启Windows的虚拟终端序列支持
+    if(enableVTMode() < 0){
+        printf("终端不支持ANSI转义序列.");
+        return -1;
+    }
+
+    // 读取Bitmap图片数据
+    if(readBitmap(BITMAP_FILE_PATH) < 0){
+        printf("文件 '" BITMAP_FILE_PATH "' 打开失败！");
+        return -1;
+    }
+
+    system("cls");  // 控制台清屏
+
+    for(int y = 0; y < height; ++y) {
+        for(int x = 0; x < width; ++x) {
+            struct Pixel pixel = pixels[x + y*width];  // 获取图片中 (x, y) 点的像素值
+
+            // printf("\033[5m");  // 闪烁效果
+            // printf("\033[1m");  // 高亮度
+            printf("\033[48;2;%d;%d;%dm", pixel.R, pixel.G, pixel.B);  // 设置文本背景色
+
+            printf("\033[38;2;%d;%d;%dm", pixel.R, pixel.G, pixel.B);  // 设置前景色(文本颜色)
+
+            printf(CHARACTER); // 打印字符
+        }
+        putchar('\n');
+    }
+
+    // 清除文字样式
+    printf("\033[0m");
+
+    free(pixels);
+
+    system("pause");
+    return 0;
+}
---
+++ Python version/colorfulCmd.py
@@ -0,0 +1,167 @@
+s = """                                                                                                                                                                
+                                               iVBORw0KGgoAAAANSUhEUgAAAGQAAAAoCAIAAACHGsgUAAAACXBIWXMAAAsTAAALEwEAmpwYAAAdP0lE QVRogb                          
+                                               WaabBlV3Xf157OfM+d3/xe9+t5VmtoMZhBCJAQQoAxCBM7HmIo27hIxSEJ    ZVcoU0USx5VyQqWMsatMSDC                            
+                     RbcCiD                    LFkIEggKaLVLam7pZ67X/eb37vz vWc+Z0/58ISgsGgLAavq3i9nn3vO+t3/Xn    uttTfSWsPLsK1hCKGXM   /       h                
+                                                F+9R/f/CBLz7lec6LN2        oEAAgQaASAsU JIaJAAEuOk19pJl+eaxlBNtpJ   qL3W4Ng3TdCzLNB3K  T       NcQv             
+                                                 sUrTu5Zie8Ihoo0          yTsBtDpZd1CEiZAS  EYoZQQgppYSUQimRFXzfNvfD9  1afeHbpSjCWOds/        +e/eu             
+                                                  WNb                   Qwi19UqU0pfpF73BNSmlVA  oBaACMUFGI9ihb7IlhLCoO2TluzY55ADd6zGYrAK    C EUKVAaS           
+                                                         21V            gAa  kMYIUyI0sixzrOZWql6j  7nvesedOnDnx3JMT3qVDs/V6Y1Kw8W7ut0Jvk   JqFNrW2c2            
+                                                       n0M8             tCpm +   xWp00tokDrnwVzQgPkmF   3c611fbG9uDrqjoTUimGFkCIE1X36qa92 l+PpX/nFN3            
+                                                     7k1            99   0+epqFG   Wlkqu1Pn95vcjz6amG6xiu  Y90YFnpJZUkps6wglDBGCcYAECepBq Q0xoQQDAVXQ           
+                                                    oh               qy   cQY3+DX    z5xZ+De/90CUiZLvVMpuvV6qV l3DZBJgEPPX3jp/2+GZWsWL  c73eTVq9ZGltc           
+                                                                     PzJ   0+effSb      ub5RtND9d2re9sX120i6NjXhtPfQ2A2eYGhIYZcQ0MbMINg gmyGDgm7Ju502           
+                                        j                             7+    Qrw87ax        YXW2WvB5lAQRg2T7t53eHJ2d9lGdxwrTc005+dnAaDVCZ   87v1wUUki1           
+                                      bd                              q/6dD8 jSX2ErC          kUlEYu56DMcEIAGA0Cm3bMgx2Y/A/ZEopjNEzpxY6/XTvninPs66ujo           
+                                    Iw                                 3zXj75 yrJ7l0T            AIAi62k6dMvfO10N0SObRRC9Tu9hfMXLj5/Jh2NtAbHtifqzvyk            
+                                  O                                     z9dHh+ rW14jQ40  +r         3Qyvx/hJAelASOFQAkhi0ISndfNcMbrVNBKa+P689eHeen1             
+                          03vv                                      5    kK3NjeuXXr6A/e4H //         YL8Vrl04vBK0IZ2ne7YfTk9V777qlVPJ+PFhxkiJAlm1t              
+                                                                    k     RJCRlFcqfg/Fqk0z  ZW         SjLEtx Cubo298dxEjPNFwPd86vKNe8UwACBP+N0+2               
+                                                                j85 YU     AQnL/QnJ+qEYEpJwe Xi4         sb5s5c WL      l5o rS7zjFNaooZrGqxaIlMN                
+                                 c                    2        6iNDH bK  FX  rSjtB4nQiqz2ivVDFCed          FXhRZkedY JT   5a    ruMlMfPr2 Kq    m               
+                                                      UX f    h8jkqOm UX fv+3  9lxcXDt9nm/0WW8owrCY 2        Tf+53/4rm1jN3LzJ    WAJIcM   otizHtl6              
+                                                       I a   KMgch  2L0hsFuBdt   FKZpVtQrLmM UANKctzrB8uZobro+UXMvdMW+Cc sm 3x     ut     9V8+1lof5             
+                                                       G/d   77Z     anZV2Vq74Uk    rbNFzXVApGQbK                      0uHb+7Ln     rl    68Mu32ki G            
+                                                       n5z   Cg       hZC IEtgm1Ehq   vkmaVVX3HsJxUsH6I2yMYxCiIoT0Ikih47av2Dg b9      T   i8OgtFv3  Z           
+                                                       m97i  1         H/   zO64/Lzl2Yf+  vzTizMKMwIyYc5//cO7/9mbdt/AtZfwn  1J S8  p   z   BKE lzW   n          
+                                                      ItRnHZ   9        5I     0E0Iyxgj      5kUFqMEoWV9pS6iMHtzGKpVK9Qay0  btRLs   1M  1  guH J6/   F          
+                            M3f                    s+ KYA  4E/ v         G6      NV2/q1LydEJv1mBQRgZlhUlecGF65j        lknX0 6J4jN   +0  J  w2  Rtd   X         
+                                               P  hytVrlx   e6ra7         kg        iBDcDMMyeI61Qhj1Leo8CzhG  o          VJcpdIn xp  TdQ M  1D  Zbx    t        
+                                              X  WGEGXM+      P opT        pu        rzs3dw7AYimy2mUhUijJH2950072xDl      46wL/gSZLn  XBCMKSUG  JQA     6       
+                                T            o  skEwhhx       jAleGu         J      zgs RJXkc50rKWtmanmpgjJTWUZxbBr         NMKpS iG   G8O0+Ob   5F      3      
+                               7        D      ND6xTi            60o7P              X   O5Jjc60VSHRno r0 CI+THBE Kg           BFGjmVQ  ijUA  J    Zg     xi     
+                              h        D    JCt7tDFdXN 9 ZWNlqd    fhaH                  aT  DM k  0        wICRq U1            EVR5Hk  iVU4 p    x       lhN   
+                              j          s1W5t+ xur   IYDntaqUKR+c   qGFjr                 K     7          QRsw3A qr         px  qiHf  e/9b3v    v1      VN84g 
+                                    bgRry5RS   XEghFcGIYJzmPEx4GPMo      LTAG                                 x6SezRyL         Zn   lR9 kzTd o     R      UJsNa 
+                                   Acao     K  A   qE8N Z8/H9Xg1Zhv           +cA                            L   bgyGAGA        LB   fd  UX7i f    Mc      2qVL 
+                                   66k      C vBLpu6dm  SNnS  hl  F                             Ial          E  aU    EtNkC     CGE   gG CM  M d   r6      5Lk  
+                                   YjZ     IgLgqlQB  TB   Y B                                               R H  i  dK   S53me  53mW   CylB   g5    Sq      E   
+                                   LIb    p1 Ec8z  Q1                                                       MIiC J7  Gs    VedsA +c5H   5D    xj3   /k          
+                              y     Af u  OawkxoTc AMU/                                                    Det HQ pQ yy3      NK8CAWi    x  vRTMO    e          
+                             a      Pp Z  IR2LIEAAwDlPs8                                                  I        vu QCQ       pMUjl8    O+cH7x     MJ         
+                             M    a  EEL  Hn9/c7KeuzYpCSq                           V935G                8       CAu0M NCD       DEoGN    A3  h      4oI        
+                                  ip  aQS CgyDGQZFGFNKtkSN                   E    AAgwUWS              F        lxpyz  YwIK      20YRC    tAU        AX         
+                                   hR  BcmJahtQ6CZHG1vbS8ub                y4aWJ1x6u2z07V                 C  DUE59os3Xf ngb      JvD0eR   lKpeL     cGPS        
+                              C     Be  Oawt40XRGWWKmjNVM0o4o               2AYdAvWcBSWfQ                8  hFKditRMfv9Du0tp    9e83Jqv   HEcxud    YW6 b       
+                              lBJEM  Ll2  bRVrOT47g7U0GEkk3oig                m4JS2qW6b                    CiXCAaKEWCUKKUMgx    GCASEAh  B  ACB E   Uhk         
+                                  iS  jjG JCsNYGwwBaSRmnBaZW2bcQ                                        Q kGQtVu9sqN3zFWPHpj  5v9++QBh1 H   VtJi    ZC+         
+                                    5chcte  J1emGnG0xNlCvll8ghXtYC9                            4 9N     KSWlUlISghJsb6swIVSW Zd g2QCNAkC    SpaT    CE          
+                                   kJDqwtJw   GIkiGF1YXJ+29u+L0yDmvmtu6UNpa                  DZ  r  tH/N5qNeYQpZmAzv8I1dVRoUqB  2pdqJz     SR                   
+                                   gzLQI20TYD  S4FJgREgoLZEgBAybV soLTQSSo8SSHL               B  hT AYc7 TUw7RUMn3fsu0 JLYv x   ZjmM8                           
+                                   yQZYeoZjAmhC MHPn 189tH+6WS9xr  s5datWrwcxkxfOcH/T65Sq rKLgQ  Am O8NRoBIIwBkautxL   Ho    f MNqd                             
+                                    QPLpOWSAwBCqCCMal     VfKf30x e5aJ7FM1u70Tz9zaWb      X3Pzc  JMMKIYQQ0lojBAXX     aR     i+8 Y                              
+                                    BFmHllI1vr8WEsMCaeQ      22TMoNxjcNMDmKeSpxw         XQgQSsF WkQkagQKtGcWMYII0I0   C       01A                              
+                                     Bl15ws4SIrhN RKa79kS    6W0 RqDE645OdNutrzx05     tXHDiZJRgj 2XIvz/MCe6fGx8sJid70 V        MAL                             
+                                     NurN9rkm+F8he rrIoJVpr   pVQh VMEFlxDk+FonV7J4x  22Tm50RY2SLlAZYaw8nGv4g  zJ9b  GA y                                       
+                                    CgheCF9ko4q8/M  n7HscZjV +JEM4oRJZgxopQ2GNal0iNn R3fsh9cf9oNB9txSNAjyVIjWZs  J5   VnJN                                      
+                                   DKqi1YTlCCLiJCm  EZHZZAgu jwvcdv2SaBjUNikFjhBACzk WYi25MDIQ1LyyLhVFW8qw0KzTAd5/  bvHVvV                                      
+                                  YDJOTdNlqaFBm0Y  5tmLq3  khZqYqSVYEQbHeCmpVt1op/XjK+iFTWgOAFLIfpKOwsEw8N1ndunR2oWNQApheWw/TT                                  
+                                MRRGkWpQnjXOLt5h2 87Vp    jkpxbCXoIKrhyLlX1z60apIAqjvQ04OOOYNgMpH/7LLz53fsGYO3w5r6e4VPMN20WmYggp                                
+                              z9BlHWKE6PgOqcA2KSANW1U    7YxhjhBBolReiHYkoLiYrJiMIY2zbbBgkgnPg2XAY75tvlDy73RmUyyXLoBpQGIbz28Yc2wzC                              
+                           eG6m7rn2i17/2LCEkFrrr WwgSv   JHTy7vn6/tmqsDgBDizJV2e1B4r tPuRWGU5VmR5pJQdnQbOTCu0zwfjaIkCC2CMbV5afLCpox                             
+            yXf          Zt12GMIoxxUQiP8Pkaajhoc 3Fp7tDhVif92OcuiMmd pQpFTQ5tV0WD253hrdbS2NxM5E+dW4mXWplfcup1DzRQRl5czBBCSKthwtOs8G2                            
+           a5bJc dqRQ7X7s2zDfJKNQ1mvlbreHCXUdix  CSFyKOY89h05OVn   fNTP+j7jw1LShXF MQLEGA3ibHVjcHD3FBdiFOXX1oL1bgYaJRlHCGzLLHI+aK9u95                           
+          P1hVP/51tPMLcxCMF06yXTmqy4+3bU3/e+t0b cuL4ZJxIDNRg    zGCWIINDaIXKywsom lF2WJsX1Tm66tomgF/IaimccjhDClgUYFVx+5dnBlTY4Jhsb8ykl                          
+          tm0QghGCLeeUUpxLxnAYpVpDrVpa70SdUb5rwqjZ4Ltulmej   IC6XfUoQxmQwjIqiAICjh6b mZsZfOawt01oLIQeDUbNREVIFcfHEmVaSScukCCFKiVJaAfRb6                         
+          3dvk1/50z/59rmFj/7xp46fX3/mWnrL0f3twdC t+pOGONKQ t9+6BwouuIwzEeUqFToRIDX     WG gmFECiGlUukB5yJDPMMRKEQRoTajBgGBWZQx2l1h9f1xJ                         
+         XNgoAC0IRsrUCIMWoYDAHq9iMp1cS4H0epBi   UUP    X 1h+V1v3NXux2UTLNNsd/qOYzu2       SSlJM94fxL5LX3Nsp/MDTa5XnmdFUcwY64ZyombFqfjWyd                        
+          U0F9Wyo7WOojxNsggjcH1LxM2iNasGu/dsR 5Xmwua     QMgNTXELKEhnFwvc9BQgRig0DbAcwBgCdxirPlYKtVUVyoaXUvADBlZQIwGSEYASUUctKomD9/EWoz1w                       
+       z5ztDXi6ZYZS5tsEFj+NMSmnblmVZi0u9sm9NTVWyrBgMw9   v2NQ7tmfyzvz0FnN+0sxHGWZ5n9VoFISyVIkjcdHDWcewfdPkV5llCCK11JxBnL63Vjs13hulY2Tx3LXE                      
+     dw2C024uC4chuVtwaQ05jpaiuFml3Zf3NrH2kbubhAHFFQGOMNWAZRcgwNFVSa6S3Wtdag8amhZSSRY EAOa6tlQJtIQ0AGgAUAMIYE9wbRmeu   B6K8v0COSRWDgnOKEOoP    k                 
+  2aj5Dh2nvMwijur6xiz1uaoVLJc16xVvFzqLz/VOjuwDnoFwcg wWJqmnAvbZtMTpbnpOqAfbhm8QmWFUWwwttaJbAM5trnSyeKkaHXiflg0 6t7Fy2     ujMDu4d2IERFtekXE  NqJ                
+BqH964eyw3GBUKACHABChFzADGgDKNSZakvCgwYSBk2uuHS 0tiM MoQHPyFd1JGX3xVjBDCILhcbEeXQ5Zgx2YUYRjmkCWpw7OK7w5GmZSq7NsYY0JwHC       fffm7N1rB9vDy/owmA1jc              
+6zZo7VcXj9fIoLLq9MIozwdPX3r5nrFl9Sa9fobIoIULIasnoBUVeJONl8+RGaNmsitBgkLielZX8gSQeEkk4zAQxbKtaNlf1jq+0lt5QDicnakANBVhjD         JgAJogQjdDCU0/LK4ueb             
+WMAJQQCTaVyahUFGr4XrglBaSZWRnI9wYn0OUYmyCgRQ0mlQg hb  I6mHrWCiYieZjpOi5JlKK  YKxXRtPOGoPu3OiRhmLouz5c61bj0w6jjMz6fcHM         QBCmK2u98calZcsD18hLN             
+u20jS3DNQdjVaHAsRw/3TpwuIIEVqtWIJzSIo+szODWZAbPI3z     YhQapmmM6OTKpnEzL/a    P0UbJMGwDMAGMAWGs4aZ73jI6sBpeXxbDERKSMI         orZf/QPsM0QSlCKSi52Ekv            
+DnCBDEqwBqUlb0XFUJqMagRag8LUWOwk673enklPgdYagZKtoJD  UtkyayLgohFLy7a/b9qXw      0mqnGOuke7bXqx  Wn0wss05AKsjy3rJfYvP         hJC2mtpOD86mbxyPGr   a+            
+2o7Bj1sj3WLMdptjIqCuYwkxkUE5BxysNEcK6IYWQFn 3ZllYmaS6fKrGwqG7KSY9TKZWTbgBF        IBVICxkAJSK  mF0EIWwag9CK+QuU5Q2A         ZWWmulW71oAC7Phe0YgJ                
+DSoJTOgyjqDiyd79ox7bnUILCRmZcGSE hwxPCu/d7h 3Q3fZf/zwdO1un/swNj8bKPVGV28vL          59rjEz    VSM/olHzk8ICgCAIHcf95O      eevpY4vo1xFoz7dLrh+g4dBG              
+mQSqfkUssUAElSaEy0zjnXcaKPzDi   tFJ06+eSphz5LRVZyzDfs3/Xu17/GLnnIMAzXNRyb             WR      bSOo+i69eXVpbXXnvfOy9E5f   Ve4nssSnLLYGut0VqI81yVKo5l             
+GxYSDRuN+eblSxuSp++66+DyZp    ArsxuLZ1uwo4buPlQ6sL0GgKI4I4RQrLKcm6ahlLIs8                     8Y7e+QTn/jETwgLYYy0yBS6  NgS35DG/EmvaieVaLwVMLIsUaV4zy            
+XTFariGx8AlUDch5zIejVaXN       wbtQAw3o6AVRMHzS+tY8L3Nhsco5DmPYyp4meKvfuu                    xj//Z579+4tS+ozc3x8b+/pHnSn4ZMElS7tkk7/fDXhgO4wrjB  6c932          
+YYM8fUH3zvzWN1v+SwxfV         hGGZTvvrwvTuaFXeLiGFQRgnGpN8fbOUWNyb104FFCC                    kKWSuRpfVBr5eAVpQxYjkFMjcG4tjeWqffe+TJ85evLKVxPF4v+   54rh         
+PYoAgQnv/3U8pnnduz           cW6nNEWIolV3cWHv80kI/51PTk9MT42vd/qe//LUvPfK                   EUIpg/d3jJ2fGp4owfPQ7p64ubnYGWTdUxHF2TnljLum0AtMyXNcQ     Qh        
+qm0axYjm24jjk3               7jGsyxaO04ISRAl6oXgEQAgRgm3b/ifdhJ9CzNJaSMUo                   efi7148fv7x4eXM0zDGjEknDdYeS/f7vvLWhN+57/x8xSjHBew7uf      v8  vv8P3
+S71+srG6IbLRwq    XF0       ycvuK5jmoZSaRRe7/evJFmwf37mv330g5954MGHTl50Ld                  t2qow1ypVtQjDGWJpl/UEICFHTpp4/Mzv27rcckMzRtLRzx8TClWtSk     mazdHBnbd
+dsjdEXYlCac6WUy   Sil       N2of/6xgCSk31gdf+rvT 33nsCs84IQQhzYscAATn5ar7                 sd975989+PjxZ1b33n5o7vDeku +ozuaoM7h84dqF0+dt23A8Z2VxA2OM       MWGMMS
+ZcVw0ST2p9z57h5f  XkY      ri3 5rOKOeh08jjmWitCM EaI2RYxGKaU2bZRrvrTszMTZ                SfuXDx3bTgINS8Yw2Nj9dmZ+q03z9/79ldZlvmTePpPwNI3nMRa66dOLjz6  ncsnTi5FUW
+  7bBsYvCDuKBlkac4Xuf0   s97PSeXjDu+t3fnL91rHUtevTPH4iH2cb6ehwFSkmMked6Jd9              zPcuxKWXKNA2lxNnLLaTyX7t3x5lzV4+f7XDA+3bN1BtzYZB2OsPBIFEKOX7Jq9ct38eUaaV
+  kkuSDfhSmluU6rs95lqZ    xnm fDQRcTtmP3+Ec+/I677rz5ZwULADq97DN/vVZ28Qfvn1            ltiaXVgW/GWRKfPrP4zLPXFpeHhNie57y4/6q1BtAIk6XFc0TFX/iju69uDh5+9OzMxDiU    
+  967kzSunnicCGrVqo1GrV  d1q  xfZKZlHEQmQagHO5sXF9ZeWszlpvuOP1q87Brz526Qg+          t3L68Y1Q79p15I43/fz4xMzGRmdlZTAc5HGcS6kwQoBgq1JCGCmtQGtKGUJ40N80Lcfzqu      
+    3O5mZ39V//y3f/9m/c8  zOB dX0l/OSfXF5YjC1DNBr+9YXFtaVrrsk9z8GYWCZjDIFWG        AHGmBBGKQXAUoqiSFeWr1XNwX/6D+//H5fdE6cuyGj4aq89S/3ZW943PlZ2HEPKNI6HcRxk      e
+     YoxptQMw/61xeeDYBPL/O6   3vu5Bdvsix7vxwEx6/ uXHn/rmN4ZJYdve7a+667bb7y     SELK4EUSRHg2htbSMPhxhkKhEgQSmjhoUAhUHP86p+uaGUjONRFAfdYe9f/c59H/7Qva8M1o0     y+K
+V     1jhEues9EaTduj+lsMFUlhl01HJ+ZNiYMtqSEMABIJYWSghd5xof94S233nr00IFhaxO  gO33fPZ5H1+Ls1fzp7TIMJSwvtwqeI4QwxpQaRZEvLDyztnHZtJlhmDjL8iKpHtu93VCHB3lEJ/    g0O/3
+Nh   zFhSvHHvvO3i9ef33PwrtXkdtevCDO89TZXlYML7Xim1Vsb7XbKDSHk4vMPOzQrlRtFkWmtLMu1LE9J8alPf80w2Qd/5a6fJqz1jcHCpSu8d8qEIWGy5gKrToFWSimtRyobKKWUkkrJLfkXPEvjgPNMa63S
+tOn  P3vnGm3rdbe83L55F11tTR8L6eHih/exXvlo58BYM khAKABiTzc2Vs+eeSJKhZTtaCdAqTpIH//eD284837j9Na7B+fLyt/7hsaV+whhDYFiWu7Z6dWV1ozw9aIzNVCt118zI3LbttdSzPCPcmYOxunR9O
+OhCyWxvLruuZ9vO1kuWy3Wuij/+9Nc4MT/0S2/8cWu9  H56Gm63hs6eWjp+4fuHiZhQVSRIwigChUqn2QyMRQghhAOA8j6JBEg2FyBEiAFAU+eTk7K/96v31xrgQWoftlc7pYK7JVs+qbsmb3CEFBwCMsZTi0pW
+ng6hNMW2tr0XhyLJMrVWS5lkw+ugvvHrCKr78+PmH  znUoBkYZwgi0RBinScyLZH7nLe+7/7ex4ZVlq0FaF7M9lUrVL41LRdbWNy5dXWn18oxjpbTJEMhcybzfW0l4CqbxBx97zxt+7qZvPrJ6cJe7f3ft5cPSi
+0vdZ08tnXh68cLF9TiWhBDTpAgB5/kWF8OwXoS1  tYullMrzJAr7UdgXotiaUlrrOAmZQbdtP9qomvff//MGMxEm3c7qqH0FMObEFjxXSmsNhOAsj7MslAKiKNNIXDr32LWrFxuNumGw/nA4WXVv21575LnlWGK
+CEcYIgFSqcwjxZnO60ZypNyZrtSYgbCBeMr  JhUSKUKCX3 7r6l0WgWhUySvDdKN7vh1x87R606Y3TY3QhHvbFtBzyz2Dljf/vEsFq2f+P+qXvvnP1RJeH3YX3+C088dXJxeXWYxJxSIkRiWTYhFGO8RWfrG2O8
+lUhorTjP8yyJ42GSjATPtAalNcLE9Er+WPPA wR1T    M+  PP fvtqGieT4+773vsu368KwZdXzo/CYZqkW1seAFoDYIQNwzAMI8/59Fzzrjv2ffYv/uKBv/ qrwaBfKpUAYSGVwQhGSEoZx9HY2OR73vu7lVo
+ d9NYBkKwoCgANCAOiJsOUEcuw9u65DSEshM QYlc u    l Jx  5/9G++/A9+ZZIalgba2riOCHVr81HM41EL6PjklPcf/+BNxw5N0BsWPCRMDwdBTgg2TWo   YNM+iOB4qrbdCLyCE8daBVlkUWRIPg1EnCL
+  ph2InjYZKkWSEs15nYM b/98JHdt91+y+03 H             Tu2r1r1r55bBEUGw/jSpfPbt007jicE   FjJP03jLPb3VMFaKc845Hwbx7bft2r9n6nWve    80dd9xJCFlZWQ6CESVESpmmqeu67    3
+   73fb/8zz8URhBFYRAESZJwzrXWSoHWCrQA0J5T2       jF/yDBsIQRjzLatEydOPPyN4+XyOGOU       IK1FpmUSDVf7G2dRMTh4dNtv/ovXvvk1M65Jm     OdEmfSsH6kv6nn2D/S5kOtVu       
+    53lNAkwYeVyk1BWFFmRJ4LnUgqllEYQp0mcF47n 3HRk77Fb99LmXKLMKOIMq2aZSc5ty9ixd+          r0dxdczwuj7E8/87lbbzmwa/deSv1yGQG00j     TV32/ngRSCMTozXcvSLC/E1Mz     s
+     v/3Yv3/Pez/w8MMPPfrIt1zXvvuut7zt7rfO79j+2f/1yPr6pmHQF6PBC/85wY7jNeqT                Y805Si0ppeeVBoPeF7/4wNnzK5XqxAvrNkJK     a0J  NIaRtlwk1ZHVuJYEjR2q    H
+      dowBwCgV1zrZXM2k5CU0ht72jv+ilNpK1REgQtlo2O6    2Fx23ghDWWknBX8j3C                  EHMyEFPTY+9423H7nrD4QO7p04uBN84sbax3     jMI     mqwbUxP+1kGqYZA+9N  dP
+       FIkgBAspO+3libHyzTcfrdebCMm8CNI0KAoOL5        y3EWMTzQ/+6ptNA5                    T  SADpJ8yzXlDGeZ5Wy6ziGVHDimYUHv/Id     9gMn       sbd4ObZdqYw5TtU0Xca
+        YbZlaq6ee+u7DD/99VpCdu27SWm6tRVprIYpO       eylLQt    NyR6P               +     H    W97/e7bfm69M7htb/XOY3OTjVLG1XI/3      zVm         43+E6/8DHzPfCVB2h
+         WgAAAAASUVORK5CYII=
+"""
+
+from PIL import Image  # pip install pillow
+import os, base64, io, ctypes
+
+
+# 开启Windows的虚拟终端序列支持
+def enableVTMode():
+  kernel32 = ctypes.windll.kernel32
+  hOut = kernel32.GetStdHandle(-11)
+  if hOut == -1:
+    print("获取窗口句柄失败！")
+    exit(0)
+
+  dwMode = ctypes.c_ulong(0)
+  if not kernel32.GetConsoleMode(hOut, ctypes.byref(dwMode)):
+      print("获取ConsoleMode失败！")
+      exit(0)
+
+  dwMode.value = dwMode.value | 0x0004
+  if not kernel32.SetConsoleMode(hOut, dwMode):
+      print("开启虚拟终端失败！")
+      exit(0)
+
+
+def colorAnsi(R:int, G:int, B:int) -> str:
+    return f"\033[38;2;{R};{G};{B}m"
+
+
+def loadImage():
+  imgPath = os.path.join(os.path.dirname(__file__),"furina.png")
+
+  if not os.path.exists(imgPath):
+    imgFile = open(imgPath, "wb")
+    imgFile.write(base64.b64decode(s))
+    imgFile.close()
+
+  img = Image.open(os.path.join(os.path.dirname(__file__),"furina.png"))
+  pixels = img.load()
+  width, height = img.size
+  return pixels, width, height
+
+
+# 程序入口
+if __name__ == "__main_":
+  
+  enableVTMode()
+  pixels, width, height = loadImage()
+
+  string_buffer = io.StringIO()
+
+  for y in range(height):
+      for x in range(width):
+          r,g,b = pixels[x, y]
+          string_buffer.write(colorAnsi(r, g, b))
+          string_buffer.write("#")
+      string_buffer.write("\n")
+
+  print(string_buffer.getvalue())
